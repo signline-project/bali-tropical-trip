edcaca86ea216604cf4d5afc7dbb4c81e4ad5f6d
Signline Studio <yourdesignerbuddy@gmail.com>
2026-05-26 13:38:08 +0700

    initial deploy bali tropical trip

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  // Ganti 'username' dengan username GitHub Anda
+  // Ganti 'bali-tropical-trip' dengan nama repositori GitHub Anda nanti
+  site: 'https://signline-project.github.io',
+  base: '/bali-tropical-trip',
+});
