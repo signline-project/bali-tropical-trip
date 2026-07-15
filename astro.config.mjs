819175fa19d6c01e044e2773a5f66fb93dd02e5b
Ahmad Shobirin <call.me.oby@gmail.com>
2026-07-15 19:59:37 +0700

    add automatic astro sitemap configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   // Ganti 'username' dengan username GitHub Anda
   // Ganti 'bali-tropical-trip' dengan nama repositori GitHub Anda nanti
   site: 'https://signline-project.github.io',
+
   base: '/bali-tropical-trip',
+  integrations: [sitemap()],
 });
